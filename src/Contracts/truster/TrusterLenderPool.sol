1a2af40aaaca723f51f7ff1be789e8121f7ed392
Nicolas Garcia <nicolasgarcia214@gmail.com>
2022-02-25 16:49:23 -0500

    3️⃣ add level 3 - Truster

---
+++ src/Contracts/truster/TrusterLenderPool.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {Address} from "openzeppelin-contracts/utils/Address.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/security/ReentrancyGuard.sol";
+
+/**
+ * @title TrusterLenderPool
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract TrusterLenderPool is ReentrancyGuard {
+    using Address for address;
+
+    IERC20 public immutable damnValuableToken;
+
+    error NotEnoughTokensInPool();
+    error FlashLoanHasNotBeenPaidBack();
+
+    constructor(address tokenAddress) {
+        damnValuableToken = IERC20(tokenAddress);
+    }
+
+    function flashLoan(
+        uint256 borrowAmount,
+        address borrower,
+        address target,
+        bytes calldata data
+    ) external nonReentrant {
+        uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
+        if (balanceBefore < borrowAmount) revert NotEnoughTokensInPool();
+
+        damnValuableToken.transfer(borrower, borrowAmount);
+        target.functionCall(data);
+
+        uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
+        if (balanceAfter < balanceBefore) revert FlashLoanHasNotBeenPaidBack();
+    }
+}
---
+++ src/test/Levels/truster/TrusterLenderPool.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+import {Utilities} from "../../utils/Utilities.sol";
+import {console} from "../../utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+import {DamnValuableToken} from "../../../DamnValuableToken.sol";
+import {TrusterLenderPool} from "../../../Contracts/truster/TrusterLenderPool.sol";
+
+contract Truster is DSTest {
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    uint256 internal constant TOKENS_IN_POOL = 1_000_000e18;
+
+    Utilities internal utils;
+    TrusterLenderPool internal trusterLenderPool;
+    DamnValuableToken internal dvt;
+    address payable internal attacker;
+
+    function setUp() public {
+        utils = new Utilities();
+        address payable[] memory users = utils.createUsers(1);
+        attacker = users[0];
+        vm.label(attacker, "Attacker");
+
+        dvt = new DamnValuableToken();
+        vm.label(address(dvt), "DVT");
+
+        trusterLenderPool = new TrusterLenderPool(address(dvt));
+        vm.label(address(trusterLenderPool), "Truster Lender Pool");
+
+        dvt.transfer(address(trusterLenderPool), TOKENS_IN_POOL);
+
+        assertEq(dvt.balanceOf(address(trusterLenderPool)), TOKENS_IN_POOL);
+
+        console.log(unicode"🧨 PREPARED TO BREAK THINGS 🧨");
+    }
+
+    function testExploit() public {
+        /** EXPLOIT START **/
+
+        /** EXPLOIT END **/
+        testAfter();
+    }
+
+    function testAfter() public {
+        assertEq(dvt.balanceOf(address(trusterLenderPool)), 0);
+        assertEq(dvt.balanceOf(address(attacker)), TOKENS_IN_POOL);
+    }
+}
