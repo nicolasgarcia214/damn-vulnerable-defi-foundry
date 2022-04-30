5c2bcf7a43e9217f824c6df9256e4118a5292a01
jeffchen006 <zhiychen@cs.toronto.edu>
2022-04-30 11:58:59 -0400

    Refactor PuppetV2Pool to 0.8.12

--- src/Contracts/puppet-v2/PuppetV2Pool.sol
+++ src/Contracts/puppet-v2/PuppetV2Pool.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0;
+pragma solidity 0.8.12;
 
-
-import "uniswap-v2-periphery/UniswapV2Library.sol";
-import "uniswap-v2-periphery/SafeMath.sol";
+import {UniswapV2Library} from "uniswap-v2-periphery/UniswapV2Library.sol";
 
 interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
@@ -16,18 +14,25 @@ interface IERC20 {
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract PuppetV2Pool {
-    using SafeMath for uint256;
 
     address private _uniswapPair;
     address private _uniswapFactory;
     IERC20 private _token;
     IERC20 private _weth;
-    
-    mapping(address => uint256) public deposits;
-        
-    event Borrowed(address indexed borrower, uint256 depositRequired, uint256 borrowAmount, uint256 timestamp);
 
-    constructor (
+    mapping(address => uint256) public deposits;
+
+    event Borrowed(
+        address indexed borrower,
+        uint256 depositRequired,
+        uint256 borrowAmount,
+        uint256 timestamp
+    );
+
+    error NotEnoughTokenBalance();
+    error TransferFailed();
+
+    constructor(
         address wethAddress,
         address tokenAddress,
         address uniswapPairAddress,
@@ -45,31 +50,47 @@ contract PuppetV2Pool {
      *         Calculations assume that WETH and borrowed token have same amount of decimals.
      */
     function borrow(uint256 borrowAmount) external {
-        require(_token.balanceOf(address(this)) >= borrowAmount, "Not enough token balance");
+
+        if(_token.balanceOf(address(this)) < borrowAmount) revert NotEnoughTokenBalance();
 
         // Calculate how much WETH the user must deposit
-        uint256 depositOfWETHRequired = calculateDepositOfWETHRequired(borrowAmount);
-        
+        uint256 depositOfWETHRequired = calculateDepositOfWETHRequired(
+            borrowAmount
+        );
+
         // Take the WETH
         _weth.transferFrom(msg.sender, address(this), depositOfWETHRequired);
 
         // internal accounting
         deposits[msg.sender] += depositOfWETHRequired;
 
-        require(_token.transfer(msg.sender, borrowAmount));
+        if (!_token.transfer(msg.sender, borrowAmount)) revert TransferFailed();
 
-        emit Borrowed(msg.sender, depositOfWETHRequired, borrowAmount, block.timestamp);
+        emit Borrowed(
+            msg.sender,
+            depositOfWETHRequired,
+            borrowAmount,
+            block.timestamp
+        );
     }
 
-    function calculateDepositOfWETHRequired(uint256 tokenAmount) public view returns (uint256) {
-        return _getOracleQuote(tokenAmount).mul(3) / (10 ** 18);
+    function calculateDepositOfWETHRequired(uint256 tokenAmount)
+        public
+        view
+        returns (uint256)
+    {
+        return (_getOracleQuote(tokenAmount) * 3) / 10**18;
     }
 
     // Fetch the price from Uniswap v2 using the official libraries
     function _getOracleQuote(uint256 amount) private view returns (uint256) {
-        (uint256 reservesWETH, uint256 reservesToken) = UniswapV2Library.getReserves(
-            _uniswapFactory, address(_weth), address(_token)
-        );
-        return UniswapV2Library.quote(amount.mul(10 ** 18), reservesToken, reservesWETH);
+        (uint256 reservesWETH, uint256 reservesToken) = UniswapV2Library
+            .getReserves(_uniswapFactory, address(_weth), address(_token));
+        return
+            UniswapV2Library.quote(
+                amount * (10**18),
+                reservesToken,
+                reservesWETH
+            );
     }
 }
