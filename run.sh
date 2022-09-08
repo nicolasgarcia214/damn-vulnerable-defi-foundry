bec4c5383c3646cdbbae0de0f9cbaf25f1073a04
0xmp <mp33crypto@gmail.com>
2022-09-08 14:58:39 -0400

    Fixed an issue of running both Puppet and PuppetV2 when running test nb 8

--- run.sh
+++ run.sh
@@ -36,7 +36,7 @@ case $1 in
     ;;
 
   puppet | 8 | pupp)
-    forge test --match-contract Puppet
+    forge test --match-contract Puppet --no-match-contract PuppetV2
     ;;
 
   puppet-v2 | 9 | pupp2)
